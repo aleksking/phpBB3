1a83c2a85c10ca16c6938761b9014aeb0b045186
aleksking <yongboang@gmail.com>
2015-06-20 18:12:04 +0800

    enable debug

--- config.php
+++ config.php
@@ -2,16 +2,16 @@
 // phpBB 3.1.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'phpbb\\db\\driver\\mysqli';
-$dbhost = 'db580929295.db.1and1.com';
-$dbport = '3306';
-$dbname = 'db580929295';
-$dbuser = 'dbo580929295';
-$dbpasswd = 'phpbb3forum123';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'phpbb3';
+$dbuser = 'root';
+$dbpasswd = '';
 $table_prefix = 'sq_';
 $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
 
 @define('PHPBB_INSTALLED', true);
-// @define('PHPBB_DISPLAY_LOAD_TIME', true);
-// @define('DEBUG', true);
-// @define('DEBUG_CONTAINER', true);
+ @define('PHPBB_DISPLAY_LOAD_TIME', true);
+ @define('DEBUG', true);
+ @define('DEBUG_CONTAINER', true);
