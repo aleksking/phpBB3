f18ecdd21ba68d6cea70f5b21eea8b00fb9071d9
aleksking <yongboang@gmail.com>
2015-06-20 19:51:32 +0800

    template

--- cache/sql_332a2c96b93660f199e54777e71d1a09.php
+++ cache/sql_332a2c96b93660f199e54777e71d1a09.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1434791583
+1434801563
 SELECT forum_id FROM sq_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
--- cache/sql_64a119cf7922c2b9c3eb1ec08651384c.php
+++ cache/sql_64a119cf7922c2b9c3eb1ec08651384c.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1434794583
+1434804563
 SELECT m.*, u.user_colour, g.group_colour, g.group_type FROM (sq_moderator_cache m) LEFT JOIN sq_users u ON (m.user_id = u.user_id) LEFT JOIN sq_groups g ON (m.group_id = g.group_id) WHERE m.display_on_index = 1
 6
 a:0:{}
